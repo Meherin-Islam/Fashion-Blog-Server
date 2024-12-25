c1e3dbea07ad8ea3b7433d604032759eaa14ddaf
Meherin-Islam <meherinislam59@gmail.com>
2024-12-25 16:44:57 +0600

    Integrated MongoDB

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7g8b9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+
+
+
 app.listen(port, () => {
   console.log(`blog is waiting: ${port}`);
 });
